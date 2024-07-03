1939170676cc3db9061af7417f548b724d10aa3c
Vinnie Post <vinnie.post1@gmail.com>
2024-07-03 01:36:51 +0000

    did rudementory splitting in 1-component

--- 3-angular/1-component/src/app/app.module.ts
+++ 3-angular/1-component/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AsideComponent } from './aside/aside.component';
+import { FunFactComponent } from './fun-fact/fun-fact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AsideComponent,
+    FunFactComponent
   ],
   imports: [
     BrowserModule
